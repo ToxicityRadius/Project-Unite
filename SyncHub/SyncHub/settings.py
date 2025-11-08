d464d36e263a4d353d94747212ff45e06eca479c
ToxicRadius <154731111+ToxicRadius@users.noreply.github.com>
2025-11-08 22:24:01 +0800

    Update settings.py

--- SyncHub/SyncHub/settings.py
+++ SyncHub/SyncHub/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'SyncHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.wayatttpewdyistfbevl',
+        'PASSWORD': 'ApocalyptoVirus',
+        'HOST': 'aws-1-ap-southeast-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
 
